42b63134109c5cb6ec33a734bab91d029564041a
ridhozahranp <ridhozahranp@students.amikom.ac.id>
2023-04-05 00:22:38 +0700

    first commit

---
+++ Mobil.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobil
+{
+    public class Mobil
+    {
+        //Field
+        private string _Warna;
+        private string _jumlahPintu;
+        private string _Merk;
+        private string _Model;
+        private string _tahunKeluar;
+        private double _Kecepatan;
+
+        //Property
+        public string Warna
+        {
+            get
+            {
+                return _Warna;
+            }
+
+            set
+            {
+                _Warna = value;
+            }
+        }
+
+        public string jumlahPintu
+        {
+            get
+            {
+                return _jumlahPintu;
+            }
+
+            set
+            {
+                _jumlahPintu = value;
+            }
+        }
+
+        public string Merk
+        {
+            get
+            {
+                return _Merk;
+            }
+
+            set
+            {
+                _Merk = value;
+            }
+        }
+        public string Model
+        {
+            get
+            {
+                return _Model;
+            }
+
+            set
+            {
+                _Model = value;
+            }
+        }
+
+        public string tahunKeluar
+        {
+            get
+            {
+                return _tahunKeluar;
+            }
+
+            set
+            {
+                _tahunKeluar = value;
+            }
+        }
+
+        public double Kecepatan
+        {
+            get
+            {
+                return _Kecepatan;
+            }
+
+            set
+            {
+                _Kecepatan = value;
+            }
+        }
+            // My Method
+        public void kecepatanMobil()
+        {
+            Console.WriteLine($"Mobil {Merk} berjalan dengan kecepatan {Kecepatan}");
+        }
+
+        public void klaksonMobil()
+        {
+            Console.WriteLine("Berklakson Tin tin tin");
+        }
+
+        public void tampilInfo()
+        {
+            Console.WriteLine($"Mobil saya berwarna {Warna}, Dengan Merk {Merk}, Dan BerModel {Model}, Keluaran Tahun {tahunKeluar}, Dengan Jumlah Pintu Sebanyak {jumlahPintu} Pintu");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mobil
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            Mobil mobil = new Mobil();
+
+            Console.WriteLine();
+
+            mobil.Warna         = "Putih";
+            mobil.Merk          = "Mustang GT";
+            mobil.Model         = "Sport";
+            mobil.tahunKeluar   = "2022";
+            mobil.jumlahPintu   = "2";
+            mobil.Kecepatan     = 1500;
+
+            mobil.kecepatanMobil();
+            mobil.klaksonMobil();
+            mobil.tampilInfo();
+        }
+    }
+}
